308940ec81b47bca3ce461f61090fb778836472b
AhmadjonM <157060690+AhmadjonM@users.noreply.github.com>
2024-03-13 15:02:01 +0500

    Add files via upload

--- form.js
+++ form.js
@@ -1,14 +1,13 @@
-function Numbers(str) {
-const num = str
-let max = 0;
-for (let i = 0; i < num.length; i++) {
-    if(num[i] > max) {
-       max = num[i];
-    }
-  } 
-
-console.log(max)
-  }
-
-Numbers([2,3,4,8,7,5,4,9,])
-
+const Numbers = (str) => { 
+    let max = 0;
+    for (let i = 0; i < str.length; i++) {
+        if(str[i] > max) {
+           max = str[i];
+        }
+    } 
+    return max
+}
+    
+console.log(Numbers([2,3,4,8,7,5,4,9]));
+    
+   
